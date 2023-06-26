5037dbb099b899fee2e4f8f3f80cf3c274f8be47
Caio Alves <78694107+caioalvesp@users.noreply.github.com>
2023-06-26 10:27:18 -0300

    criação do modal de login

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initTabNav from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
+initModal();
